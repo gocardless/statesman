8417a6de71354a45e3da43b2504fbd4540454171
Andrew Appleton <andrew@floatleft.com>
2013-09-11 10:53:48 +0100

    Basic transition accepts metadata

--- lib/statesman/transition.rb
+++ lib/statesman/transition.rb
@@ -3,11 +3,13 @@ module Statesman
     attr_accessor :created_at
     attr_accessor :from
     attr_accessor :to
+    attr_accessor :metadata
 
-    def initialize(from, to)
+    def initialize(from, to, metadata = nil)
       @created_at = Time.now
       @from = from
       @to = to
+      @metadata = metadata
     end
   end
 end
--- spec/statesman/transition_spec.rb
+++ spec/statesman/transition_spec.rb
@@ -9,5 +9,12 @@ describe Statesman::Transition do
     specify { expect(create.from).to equal(from) }
     specify { expect(create.to).to equal(to) }
     specify { expect(create.created_at).to be_a(Time) }
+
+    context "with metadata passed" do
+      let(:metadata) { { some: :hash } }
+      let(:create) { Statesman::Transition.new(from, to, metadata) }
+
+      specify { expect(create.metadata).to eq(metadata) }
+    end
   end
 end
