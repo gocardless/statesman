ab06fa3f1988914c8d5dd55cfe6aacb8183d7152
Daniel Luxemburg <daniel.luxemburg@gmail.com>
2013-10-31 09:22:42 -0400

    mongoid model generator class

--- lib/generators/statesman/mongoid_transition_generator.rb
+++ lib/generators/statesman/mongoid_transition_generator.rb
@@ -1,1 +1,31 @@
-mongoid_transition_generator.rb
+require "rails/generators"
+
+module Statesman
+  class TransitionGenerator < Rails::Generators::Base
+    desc "Create a Mongoid-based transition model with the required attributes"
+
+    argument :parent, type: :string, desc: "Your parent model name"
+    argument :klass, type: :string, desc: "Your transition model name"
+
+    source_root File.expand_path('../templates', __FILE__)
+
+    def create_model_file
+      template("mongoid_transition_model.rb.erb", model_file_name)
+    end
+
+    private
+
+    def model_file_name
+      "app/models/#{klass.underscore}.rb"
+    end
+
+    def collection_name
+      klass.underscore.pluralize
+    end
+
+    def parent_id
+      parent.underscore + "_id"
+    end
+
+  end
+end
