39c7bee32dcc8689e2a141a3aa94f44e0cd86c42
Andrew Appleton <andrew@floatleft.com>
2014-03-24 17:14:19 +0000

    Add a model mixin and define .with_states

---
+++ lib/statesman/adapters/active_record_model.rb
@@ -0,0 +1,35 @@
+module Statesman
+  module Adapters
+    module ActiveRecordModel
+
+      def with_states(*states)
+        states = Array(states)
+        self.joins(transition_name)
+            .joins(transition_join)
+            .where("#{transition_name}.to_state" => states)
+            .where("transition2.id" => nil)
+      end
+
+      private
+
+      def transition_class
+        raise NotImplementedError, "A transition_class method should be" +
+                                   "defined on the model"
+      end
+
+      def transition_name
+        transition_class.table_name.to_sym
+      end
+
+      def model_foreign_key
+        self.reflections[transition_name].foreign_key
+      end
+
+      def transition_join
+        "LEFT OUTER JOIN #{transition_name} transition2
+           ON transition2.#{model_foreign_key} = #{self.table_name}.id
+           AND transition2.sort_key > #{transition_name}.sort_key"
+      end
+    end
+  end
+end
---
+++ spec/statesman/adapters/active_record_model_spec.rb
@@ -0,0 +1,45 @@
+require "spec_helper"
+
+describe Statesman::Adapters::ActiveRecordModel do
+  before do
+    prepare_model_table
+    prepare_transitions_table
+  end
+
+  before do
+    MyActiveRecordModel.send(:extend, Statesman::Adapters::ActiveRecordModel)
+    MyActiveRecordModel.class_eval do
+      def self.transition_class
+        MyActiveRecordModelTransition
+      end
+    end
+  end
+
+  describe ".with_states" do
+    let(:model) do
+      model = MyActiveRecordModel.create
+      model.my_active_record_model_transitions.create(to_state: :state_a)
+      model
+    end
+
+    context "given a single state" do
+      subject { MyActiveRecordModel.with_states(:state_a) }
+
+      it { should include model }
+    end
+
+    context "given multiple states" do
+      subject { MyActiveRecordModel.with_states(:state_a, :state_b) }
+
+      let(:other_model) do
+        model = MyActiveRecordModel.create
+        model.my_active_record_model_transitions.create(to_state: :state_b)
+        model
+      end
+
+      it { should include model }
+      it { should include other_model }
+    end
+  end
+
+end
