59a53d5a4d1ec76f49b9ba92806fb6f8fe7f8432
Paul Springett <paul@springett.me>
2014-05-16 16:52:16 +0100

    Add failing spec

--- spec/statesman/machine_spec.rb
+++ spec/statesman/machine_spec.rb
@@ -440,6 +440,18 @@ describe Statesman::Machine do
       end
       it { should be_false }
     end
+
+    context "when a non statesman exception is raised" do
+      before do
+        instance.stub(:transition_to!).and_raise(RuntimeError, 'user defined exception')
+      end
+
+      it "should raised not rescue the exception" do
+        expect {
+          instance.transition_to(:some_state, metadata)
+        }.to raise_error(RuntimeError, 'user defined exception')
+      end
+    end
   end
 
   shared_examples "a callback filter" do |definer, phase|
