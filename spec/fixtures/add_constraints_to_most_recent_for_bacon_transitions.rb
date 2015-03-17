a19b71cb9ba64c4cbe99f368ac5541021ff55906
Grey Baker <greysteil@gmail.com>
2015-03-17 18:29:39 +0000

    Use change_column_null over change_column

--- spec/fixtures/add_constraints_to_most_recent_for_bacon_transitions.rb
+++ spec/fixtures/add_constraints_to_most_recent_for_bacon_transitions.rb
@@ -3,11 +3,11 @@ class AddConstraintsToMostRecentForBaconTransitions < ActiveRecord::Migration
 
   def up
     add_index :bacon_transitions, [:bacon_id, :most_recent], unique: true, where: "most_recent", name: "index_bacon_transitions_parent_most_recent", algorithm: :concurrently
-    change_column :bacon_transitions, :most_recent, :boolean, null: false
+    change_column_null :bacon_transitions, :most_recent, false
   end
 
   def down
     remove_index :bacon_transitions, name: "index_bacon_transitions_parent_most_recent"
-    change_column :bacon_transitions, :most_recent, :boolean, null: true
+    change_column_null :bacon_transitions, :most_recent, true
   end
 end
