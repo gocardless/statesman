cd0ebfb337828707f7edc6c780cb11a2a71f50b4
Chris Ewald <chrisewald@gmail.com>
2016-07-05 00:42:11 -0400

    add transition_helper

--- lib/statesmin.rb
+++ lib/statesmin.rb
@@ -1,7 +1,8 @@
 module Statesmin
-  autoload :Machine,    'statesmin/machine'
-  autoload :Callback,   'statesmin/callback'
-  autoload :Guard,      'statesmin/guard'
-  autoload :Version,    'statesmin/version'
+  autoload :Callback,         'statesmin/callback'
+  autoload :Guard,            'statesmin/guard'
+  autoload :Machine,          'statesmin/machine'
+  autoload :TransitionHelper, 'statesmin/transition_helper'
+  autoload :Version,          'statesmin/version'
   require 'statesmin/railtie' if defined?(::Rails::Railtie)
 end
