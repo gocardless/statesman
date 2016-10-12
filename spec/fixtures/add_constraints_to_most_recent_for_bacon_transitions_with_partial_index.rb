148c0de21853012febb0759ff4b950ea6ff93a63
Tadayuki Onishi <tt.tanishi100@gmail.com>
2016-10-12 10:53:42 +0900

    Fix Migration Deprecation Warning on Rails 5

--- spec/fixtures/add_constraints_to_most_recent_for_bacon_transitions_with_partial_index.rb
+++ spec/fixtures/add_constraints_to_most_recent_for_bacon_transitions_with_partial_index.rb
@@ -1,4 +1,4 @@
-class AddConstraintsToMostRecentForBaconTransitions < ActiveRecord::Migration
+class AddConstraintsToMostRecentForBaconTransitions < ActiveRecord::Migration[4.2]
   disable_ddl_transaction!
 
   def up
--- spec/fixtures/add_most_recent_to_bacon_transitions.rb
+++ spec/fixtures/add_most_recent_to_bacon_transitions.rb
@@ -1,4 +1,4 @@
-class AddMostRecentToBaconTransitions < ActiveRecord::Migration
+class AddMostRecentToBaconTransitions < ActiveRecord::Migration[4.2]
   def up
     add_column :bacon_transitions, :most_recent, :boolean, null: true
   end
