9a93eab976217f7bd67a37fa3e31a12800c0e3bc
Dylan Hoefsloot <dylan.hoefsloot@abletech.co.nz>
2023-01-27 14:49:03 +1300

    Add transition for initial state
    This will allow the state machine to track the initial state for the machine, this is important for 3 reasons:
    - It allows us to keep a record of how long the state machine spent in the initial state
    - If the initial state configuration changes after the state machine transitions from the initial state we still have an accurate record of what the initial state was
    - If the initial state configuration changes before the state machine transitions from the initial state we still have an accurate record of what the initial state is

--- lib/statesman/machine.rb
+++ lib/statesman/machine.rb
@@ -233,12 +233,18 @@ module Statesman
     def initialize(object,
                    options = {
                      transition_class: Statesman::Adapters::MemoryTransition,
+                     initial_transition: false
                    })
       @object = object
       @transition_class = options[:transition_class]
       @storage_adapter = adapter_class(@transition_class).new(
         @transition_class, object, self, options
       )
+
+      if options[:initial_transition]
+        @storage_adapter.create(nil, self.class.initial_state, {})
+      end
+
       send(:after_initialize) if respond_to? :after_initialize
     end
 
