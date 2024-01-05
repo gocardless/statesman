10844907f2279bc814d7187b486707814dece506
Dave Gudge <dave@gudge.uk>
2024-01-05 11:08:26 +0000

    `ensure_inheritance` condition
    `subclasses` was added as part of Ruby 3.1
    https://ruby-doc.org/core-3.1.0/Class.html#method-i-subclasses
    
    Add a `respond_to?` to `base` to ensure that Ruby 3.0 will continue to
    work as expected.
    
    https://github.com/gocardless/statesman/pull/511#discussion_r1442670189

--- lib/statesman/adapters/active_record_queries.rb
+++ lib/statesman/adapters/active_record_queries.rb
@@ -39,7 +39,7 @@ module Statesman
         end
 
         def included(base)
-          ensure_inheritance(base) unless base.subclasses.none?
+          ensure_inheritance(base) if base.respond_to?(:subclasses) && base.subclasses.any?
 
           query_builder = QueryBuilder.new(base, **@args)
 
